0e4197e5c3b065c928e9574b2aedfd47601e2ea6
Stefan Lahtinen <stefan.lahtinen@gmail.com>
2024-12-20 03:25:24 +0100

    add form to CreateSessionPage

--- src/pages/CreateSessionPage.jsx
+++ src/pages/CreateSessionPage.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreateSessionPage = () => {
+    const [title, setTitle] = useState("Best Fruit");
+    const [candidates, setCandidates] = useState(["Banana", "Mango"]);
+    const [duration, setDuration] = useState(36000);
+
   return (
     <div>
         <h2>Create a Voting Session</h2>
+
+        <form>
+          <label>Title: </label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label>Candidates: </label>
+          <input
+            type="text"
+            value={candidates}
+            onChange={(e) => setCandidates(e.target.value)}
+          />
+          <label>Duration: </label>
+          <input
+            type="number"
+            value={duration}
+            onChange={(e) => setDuration(e.target.value)}
+          />
+        <button type="submit">Create</button>
+      </form>
       
     </div>
   )
