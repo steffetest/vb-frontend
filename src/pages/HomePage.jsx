fe61a46d4f3ccbda4ceb94ecf165a795a8bccb5e
Stefan Lahtinen <stefan.lahtinen@gmail.com>
2024-12-20 01:48:36 +0100

    add router and start on HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+        <h1>Welcome to VotingBlock</h1>
+        <button>Create Voting Session</button>
+        <button>View Voting Sessions</button>
+      
+    </div>
+  )
+}
+
+export default HomePage
